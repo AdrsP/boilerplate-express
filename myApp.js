00976f1090c298de94b0507f359d1cde7b1ad0ad
AdrsP <adrsparr@gmail.com>
2024-05-02 08:55:34 -0400

    Finalizado papu

--- myApp.js
+++ myApp.js
@@ -1,9 +1,26 @@
 require('dotenv').config()
 
+let bodyParser = require('body-parser')
+
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+// intento de uso de un "post"
+app.post("/eshoradepost", (req, res) => {
+    res.send(req.body.hello)
+})
+
+// post de name
+
+app.post("/name", (req, res)=>{
+    res.json({name: `${req.body.first} ${req.body.last}`})
+})
 
 // root-level middleware el cual a todas las solicitudes desglosa el metodo (GET, PUT, POST etc) + la ruta con que los llaman + el ip del que se llama 
 // se debe definir antes que todos los metodos para que pueda leerlos si se define luego no tiene acceso a esos metodos
@@ -39,6 +56,19 @@ app.get("/now", (req, res, next) => {
 }, (req, res) => {
     res.json({"time": req.time})
 } )
+
+// Crea un servidor echo montado en la ruta GET /:word/echo , responde con un JSON con {echo: word}
+
+app.get("/:word/echo", (req, res) => {
+    res.json({"echo": req.params.word})
+})
+
+// Crea un endpoint de API montado en la ruta GET /name, responde con un JSON con {name: 'firstname lastname'}
+// firstname y lastname deben estar codificados dentro de la ruta en un query string like ?first=firstname&last=lastname
+
+app.get("/name", (req, res) => {
+    res.json({"name": `${req.query.first} ${req.query.last}`})
+})
 
 
 
